178c5e15678799342f66a96666b9dbaf5780af59
Ahmed Hegazy <ahmedhegazy26105@gmail.com>
2026-03-24 08:14:42 +0200

    Added api url to app

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -1,5 +1,2 @@
-// Set this in production to your backend base URL, e.g. "https://api.example.com"
-// Local (for your machine)
-// Production (on Vercel)
-window.API_BASE_URL = "";
+window.API_BASE_URL = "https://dr-hegazy-clinic.vercel.app/";
 
